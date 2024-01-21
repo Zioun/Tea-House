c17d210c28e93f154dcf131da3c13982234de6b8
Zioun <jionkhan0@gmail.com>
2024-01-21 08:27:56 +0600

    tea house project start

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    //...
+    plugins: [require("daisyui")],
+  }
+
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
