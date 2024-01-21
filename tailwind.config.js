27f5878f3e8e20998ac8d96cb7d9166ab8cac6c3
Zioun <jionkhan0@gmail.com>
2024-01-21 10:49:03 +0600

    header section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
-module.exports = {
-    //...
-    plugins: [require("daisyui")],
-  }
-
-  /** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
@@ -11,3 +6,14 @@ module.exports = {
     },
     plugins: [],
   }
+
+  module.exports = {
+    theme: {
+      extend: {
+        transitionDuration: {
+          '2000': '2000ms',
+        }
+      }
+    }
+  }
+  
